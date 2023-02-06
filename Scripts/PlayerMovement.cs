411df2094a2f698efd1ba00294bdf3926f8eeec4
ttipton2 <61717127+ttipton2@users.noreply.github.com>
2023-02-05 23:46:51 -0800

    stationary npc dialog
    still needs work

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {   
+    public enum PlayerState{
+        move,
+        attack,
+        interact
+    }
     public float playerSpeed;
+    private PlayerState currentState;
     private Rigidbody2D playerRidgedbody;
+    private Animator playerAnimator;
     private Vector3 playerMovementChange;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentState = PlayerState.move;
         playerRidgedbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
         
     }
 
@@ -23,16 +32,39 @@ public class PlayerMovement : MonoBehaviour
         playerMovementChange.y = Input.GetAxisRaw("Vertical");
         Debug.Log(playerMovementChange);
 
-        if(playerMovementChange != Vector3.zero){
-            MoveCharacter();
+        if(Input.GetButtonDown("Interact") && currentState != PlayerState.interact){
+            StartCoroutine(InteractCoRoutine());//still need to write the interactCoRoutine method
         }
-            
+        else if(currentState == PlayerState.move){
+            UpdateAnimationAndMove();
+        }
     }
+
+        private IEnumerator InteractCoRoutine(){
+            playerAnimator.SetBool("interacting", true);
+            currentState = PlayerState.interact;
+            yield return null;
+            playerAnimator.SetBool("interacting", false);
+            yield return new WaitForSeconds(0.3f);
+            currentState = PlayerState.move;
+        }
+
+        void UpdateAnimationAndMove(){
+            if(playerMovementChange != Vector3.zero){
+                MoveCharacter();
+                playerAnimator.SetFloat("setX", playerMovementChange.x);
+                playerAnimator.SetFloat("setY", playerMovementChange.y);
+                playerAnimator.SetBool("moving", true);
+            }
+            else{
+                playerAnimator.SetBool("moving", false);
+            }
+        }
+        
 
     void MoveCharacter(){
         playerRidgedbody.MovePosition(transform.position + playerMovementChange *
             playerSpeed * Time.deltaTime);
-
-
     }
 }
+
